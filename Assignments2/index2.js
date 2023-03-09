e4ff34d9777c2feb373e4efa466a5c4d552a018e
Van Vu <vanvn.vu@gmail.com>
2023-03-09 11:18:55 +0700

    Added assignments 2

---
+++ Assignments2/index2.js
@@ -0,0 +1,8 @@
+let length = +prompt("Nhập chiều dài hình chữ nhật");
+let width = +prompt("Nhập chiều rộng hình chữ nhật");
+
+console.log(length, width);
+
+area = length * width;
+
+document.write('Chiều dài hình chữ nhật là: ' + length + "<br>" + 'Chiều rộng hình chữ nhật là: ' + width + "<br>" + 'Diện tích hình chữ nhật là: ' + area);
